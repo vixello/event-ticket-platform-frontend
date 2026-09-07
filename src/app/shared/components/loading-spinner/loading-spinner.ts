9813ad06687e8dd75867e38d6d4041086e408af3
Izabela K <aizeilla12344@gmail.com>
2026-09-08 00:06:52 +0200

    feat(shared): add loading-spinner component

---
+++ src/app/shared/components/loading-spinner/loading-spinner.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.html',
+})
+export class LoadingSpinner {}
