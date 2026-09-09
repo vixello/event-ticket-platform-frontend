5ac283c3beae58d89949dda6273f721b507ec54d
Izabela K <aizeilla12344@gmail.com>
2026-09-09 14:30:07 +0200

    feat(events): add event-details component

---
+++ src/app/features/events/pages/event-details/event-details.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-event-details',
+  templateUrl: './event-details.html',
+})
+export class EventDetails {}
