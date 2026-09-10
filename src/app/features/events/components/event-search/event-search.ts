0ff922abff16c62ec26e876010f0cc638ef4ee80
Izabela K <aizeilla12344@gmail.com>
2026-09-10 22:57:50 +0200

    feat(shared): add event-search markup and TS imports

--- src/app/features/events/components/event-search/event-search.ts
+++ src/app/features/events/components/event-search/event-search.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-event-search',
   templateUrl: './event-search.html',
 })
-export class EventSearch {}
+export class EventSearch {
+  searchTerm = '';
+
+  onSearch(): void{
+    console.log('Searching for: ', this.searchTerm);
+  }
+}
