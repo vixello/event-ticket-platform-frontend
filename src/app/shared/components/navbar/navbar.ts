3a9f5906b624458f8491b115dce5c167382cba7e
Izabela K <aizeilla12344@gmail.com>
2026-09-10 22:57:20 +0200

    feat(shared): add navbar markup and TS imports

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-navbar',
   templateUrl: './navbar.html',
 })
