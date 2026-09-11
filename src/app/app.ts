f4faa21da7f59470628967e677a3224753b20128
Izabela K <aizeilla12344@gmail.com>
2026-09-11 23:37:30 +0200

    feat(layout): add navbar, footer and main app wrapper

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./shared/components/navbar/navbar";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
