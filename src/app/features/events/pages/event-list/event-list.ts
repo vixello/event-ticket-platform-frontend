0a97a40b70a9306d51be305f95fab06cc002726c
Izabela K <aizeilla12344@gmail.com>
2026-09-11 23:43:06 +0200

    feat(events): add search binding and event fetching in event-list

--- src/app/features/events/pages/event-list/event-list.ts
+++ src/app/features/events/pages/event-list/event-list.ts
@@ -2,10 +2,29 @@ import { Component } from '@angular/core';
 import { EventSearch } from '../../components/event-search/event-search';
 import { EventGrid } from '../../components/event-grid/event-grid';
 import { EventFilters } from '../../components/event-filters/event-filters';
+import { EventService } from '../../services/event.service';
+import { Event } from '../../models/event';
 
 @Component({
   imports: [EventSearch, EventFilters, EventGrid],
   selector: 'app-event-list',
   templateUrl: './event-list.html',
 })
-export class EventList {}
+export class EventList {
+
+  events: Event[] = [];
+
+  constructor(
+    private eventService: EventService
+  ) {
+    this.events = this.eventService.getEvents();
+  };
+
+  fetchEvents() {
+    this.eventService.getEvents();
+  }
+  
+  onSearch(searchQuery: String) {
+    this.events = this.eventService.searchEvents(searchQuery);
+  }
+}
