4a7108b6384cbc73d4f47c9bcda48503522fe602
Izabela K <aizeilla12344@gmail.com>
2026-09-11 23:35:47 +0200

    feat(routes): add routes for home page of events

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'events',
+        pathMatch: 'full'
+    },
+    {
+        path: 'events',
+        loadComponent: () => import('./features/events/pages/event-list/event-list')
+            .then((m) => m.EventList)
+    },
+    {
+        path: 'events/:id',
+        loadComponent: () => import('./features/events/pages/event-details/event-details')
+            .then((m) => m.EventDetails),
+    },
+    {
+        path: '**',
+        redirectTo: 'events',
+    },
+];
