bcf3d0a0835b0d8453d9b91547996494c2ce1959
Izabela K <aizeilla12344@gmail.com>
2026-09-11 23:39:00 +0200

    feat(events): implement event-card template and input binding

--- src/app/features/events/components/event-card/event-card.ts
+++ src/app/features/events/components/event-card/event-card.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Event } from '../../models/event';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, DatePipe],
   selector: 'app-event-card',
   templateUrl: './event-card.html',
 })
-export class EventCard {}
+
+export class EventCard {
+  event = input.required<Event>();
+}
