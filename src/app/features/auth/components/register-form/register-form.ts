c108bf4d228d6ddd9e2562ee70059056056c196a
Izabela K <aizeilla12344@gmail.com>
2026-09-14 23:57:14 +0200

    feat(auth): add register form component

---
+++ src/app/features/auth/components/register-form/register-form.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-register-form',
+  templateUrl: './register-form.html',
+})
+export class RegisterForm {}
