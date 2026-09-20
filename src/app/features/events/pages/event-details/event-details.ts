15bf31aa50dcec17346a471bb2eea831ad188532
Izabela K <aizeilla12344@gmail.com>
2026-09-20 23:51:24 +0200

    feat(event): implement event details page

--- src/app/features/events/pages/event-details/event-details.ts
+++ src/app/features/events/pages/event-details/event-details.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Event } from '../../models/event';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from '../../services/event.service';
 
 @Component({
   imports: [],
   selector: 'app-event-details',
   templateUrl: './event-details.html',
 })
-export class EventDetails {}
+export class EventDetails {
+
+  event: Event | null = null;
+
+  constructor(private route: ActivatedRoute,
+    private eventService: EventService
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) {
+      return;
+    }
+
+    this.event = this.eventService.getById(id) ?? null;
+    console.log(id);
+    console.log('event:', this.event);
+  }
+}
