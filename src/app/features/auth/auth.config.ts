2c3452d34846e79203d46cb1bff04c8437b4fb47
Izabela K <aizeilla12344@gmail.com>
2026-09-22 00:08:52 +0200

    fix(auth): edit in auth.config

--- src/app/features/auth/auth.config.ts
+++ src/app/features/auth/auth.config.ts
@@ -1,4 +1,6 @@
-const keycloakConfig = {
+import { KeycloakConfig } from "keycloak-js";
+
+export const keycloakConfig: KeycloakConfig = {
   url: 'http://localhost:9090',
   realm: 'event-ticket-platform',
   clientId: 'event-ticket-platform-app',
