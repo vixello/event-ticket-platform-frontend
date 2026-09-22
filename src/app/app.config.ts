4a0afa5c3df926e2bcb4c34ea8c62d952c94ac36
Izabela K <aizeilla12344@gmail.com>
2026-09-22 23:58:08 +0200

    feat(auth): initialize Keycloak during app bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { AuthService } from './features/auth/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(),
+    provideAppInitializer(() => {
+      const authService = inject(AuthService);
+
+      return authService.init();
+    }),
   ]
 };
